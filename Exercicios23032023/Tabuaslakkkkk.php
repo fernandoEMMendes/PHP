7ce4f9b7c708bf4cd4a9cf6e9bb44196270f1b09
FernandoMendes <fernandoeduardomottamendes@gmail.com>
2023-03-30 09:54:37 -0300

    sdsadfs

---
+++ Exercicios23032023/Tabuaslakkkkk.php
@@ -0,0 +1,15 @@
+<?php
+
+$tabuada = 9;
+
+while ($contador <= 10) {
+    $resultado = $tabuada * $contador;
+    echo "($tabuada) x ($contador) = ($resultado) <br />";
+
+    $contador = $contador +1;
+}
+
+
+
+
+?>
