71fa85c8013150ff9e66a6625b13f0537f25f9ab
FernandoMendes <fernandoeduardomottamendes@gmail.com>
2023-08-17 09:57:46 -0300

    17/08/23 - Enviando para o DB

--- sistema/bd.php
+++ sistema/bd.php
@@ -3,7 +3,24 @@
 
 
 if ($_POST["email"] == $_POST["emailconfirm"] and $_POST["senha"] == $_POST["senhaconfirm"]) {
-    echo "usuario e senha são iguais";
+    $pdo = new PDO("mysql:host=localhost;dbname=aulasphp", "root", "");
+
+    $sql = $pdo->prepare("INSERT INTO `usuario` values (null,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+    $sql->execute(
+        array(
+            $_POST["nome"],
+            $_POST["sobrenome"],
+            $_POST["endereco"],
+            $_POST["numero"],
+            $_POST["cidade"],
+            $_POST["estado"],
+            $_POST["rg"],
+            $_POST["cpf"],
+            $_POST["data_nas"],
+            sha1($_POST["sexo"]),
+        )
+    );
 } else {
     echo "usuario e senha são diferentes";
 }
--- sistema/cadastro.php
+++ sistema/cadastro.php
@@ -18,118 +18,120 @@
     </nav>
 
     <div class="container text-center">
-        <form>
-            <h2>Cadastro de usuários</h2>
-            <form action="bd.php" method="post">
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <input class="form-control " type="text" name="nome" id="" placeholder="Nome" required>
-                    </div>
+        <h2>Cadastro de usuários</h2>
+        <form action="bd.php" method="post">
 
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="sobrenome" id="" placeholder="Sobrenome" required>
-                    </div>
-                </div class="row">
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control " type="text" name="nome" id="" placeholder="Nome" required>
+                </div>
 
-                <br />
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="sobrenome" id="" placeholder="Sobrenome" required>
+                </div>
+            </div class="row">
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <input class="form-control " type="email" name="email" id="" placeholder="Email" required>
-                    </div>
+            <br />
 
-                    <div class="col-md-6">
-                        <input class="form-control" type="password" name="senha" id="" placeholder="Senha" required>
-                    </div>
-                </div class="row">
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control " type="email" name="email" id="" placeholder="Email" required>
+                </div>
 
-        
+                <div class="col-md-6">
+                    <input class="form-control" type="password" name="senha" id="" placeholder="Senha" required>
+                </div>
+            </div class="row">
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <input class="form-control " type="email" name="emailconfirm" id="" placeholder="Confirmar Email" required>
-                    </div>
 
-                    <div class="col-md-6">
-                        <input class="form-control" type="password" name="senhaconfirm" id="" placeholder="Confirmar Senha" required>
-                    </div>
-                </div class="row">
 
-                <br />
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control " type="email" name="emailconfirm" id="" placeholder="Confirmar Email"
+                        required>
+                </div>
+
+                <div class="col-md-6">
+                    <input class="form-control" type="password" name="senhaconfirm" id="" placeholder="Confirmar Senha"
+                        required>
+                </div>
+            </div class="row">
+
+            <br />
+
+            <div class="col-md-12">
+                <select required name="sexo" required id="" required>
+                    <option value="">Selecione um</option>
+                    <option value="fem">Feminino</option>
+                    <option value="mas">Masculino</option>
+                    <option value="indefinido">Prefiro não responder</option>
+                </select>
+            </div>
+
+            <br />
+
+
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="endereco" id="" placeholder="Endereço" required>
+                </div>
+
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="numero" id="" placeholder="N°" required>
+                </div>
+            </div>
+
+
+
+
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="cidade" id="" placeholder="Cidade" required>
+                </div>
+
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="estado" id="" placeholder="Estado" required>
+                </div>
+            </div>
+
+            <br />
+
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="rg" id="" placeholder="RG" required>
+                </div>
+
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="cpf" id="" placeholder="CPF" required>
+                </div>
 
                 <div class="col-md-12">
-                    <select required name="sexo" required id="" required>
-                        <option value="">Selecione um</option>
-                        <option value="fem">Feminino</option>
-                        <option value="mas">Masculino</option>
-                        <option value="indefinido">Prefiro não responder</option>
-                    </select>
+                    <input class="form-control" type="text" name="data_nas" id="" placeholder="Data de Nascimento"
+                        required>
                 </div>
+            </div>
 
-                <br />
-
-
-                <div class="row">
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="endereco" id="" placeholder="Endereço" required>
-                    </div>
-
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="numero" id="" placeholder="N°" required>
-                    </div>
-                </div>
+            <br />
 
 
 
-
-                <div class="row">
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="cidade" id="" placeholder="Cidade" required>
-                    </div>
-
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="estado" id="" placeholder="Estado" required>
-                    </div>
-                </div>
-
-                <br />
-
-                <div class="row">
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="rg" id="" placeholder="RG" required>
-                    </div>
-
-                    <div class="col-md-6">
-                        <input class="form-control" type="text" name="cpf" id="" placeholder="CPF" required>
-                    </div>
-
-                    <div class="col-md-12">
-                        <input class="form-control" type="text" name="data_nas" id="" placeholder="Data de Nascimento"
-                            required>
-                    </div>
-                </div>
-
-                <br />
+            <div>
+                <input class="btn btn-primary btn-lg" type="submit" value="cadastrar">
+            </div>
+        </form>
 
 
 
-                <div>
-                    <input class="btn btn-primary btn-lg" type="submit" value="cadastrar">
-                </div>
-            </form>
-    </div>
+        <footer class="footer py-3 bg-black fixed-bottom">
+            <div class="container text-center">
+                <h3 class="text-light">Todos os direitos reservados - 2023</h3>
+            </div>
+        </footer>
 
-
-    <footer class="footer py-3 bg-black fixed-bottom">
-        <div class="container text-center">
-            <h3 class="text-light">Todos os direitos reservados - 2023</h3>
-        </div>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+            crossorigin="anonymous"></script>
 </body>
 
 </html>
